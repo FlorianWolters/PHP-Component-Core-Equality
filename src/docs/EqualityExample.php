e9356245ac0aae075791d845b407a81eebbebd71
Florian Wolters <wolters.fl@gmail.com>
2013-01-07 16:44:50 +0100

    Updated example class.

--- src/docs/EqualityExample.php
+++ src/docs/EqualityExample.php
@@ -1,62 +1,113 @@
 <?php
 use FlorianWolters\Component\Core\EqualityInterface;
 use FlorianWolters\Component\Core\EqualityUtils;
+use FlorianWolters\Mock\EqualityCustomImplMock;
+use FlorianWolters\Mock\EqualityDefaultImplMock;
 
-require '../../vendor/autoload.php';
-require 'EqualityDefaultImpl.php';
-require 'EqualityCustomImpl.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
-exit(main());
-
-function main()
+/**
+ * A simple command line application to demonstrate the component
+ * **FlorianWolters\Component\Core\Equality**.
+ *
+ * @author    Florian Wolters <wolters.fl@gmail.com>
+ * @copyright 2012-2013 Florian Wolters
+ * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
+ * @link      http://github.com/FlorianWolters/PHP-Component-Core-Equality
+ * @since     Class available since Release 0.2.0
+ */
+final class EqualityExample
 {
-    demonstrateDefaultImplementation();
-    demonstrateCustomImplementation();
+    /**
+     * @param integer $argc
+     * @param array   $argv
+     *
+     * @return integer
+     */
+    public static function main($argc, array $argv = [])
+    {
+        $self = new self;
+        $self->demonstrateDefaultImplementation();
+        $self->demonstrateCustomImplementation();
+
+        return 0;
+    }
+
+    /**
+     * @return void
+     */
+    private function demonstrateDefaultImplementation()
+    {
+        $firstObj = new EqualityDefaultImplMock;
+        $secondObj = new EqualityDefaultImplMock;
+        $thirdObj = new EqualityDefaultImplMock;
+
+        $this->writeLine('Default implementation:');
+        $this->output($firstObj, $secondObj, $thirdObj);
+    }
+
+    /**
+     * @return void
+     */
+    private function demonstrateCustomImplementation()
+    {
+        $firstObj = new EqualityCustomImplMock;
+        $secondObj = new EqualityCustomImplMock(false);
+        $thirdObj = new EqualityCustomImplMock(0);
+
+        $this->writeLine('Custom implementation:');
+        $this->output($firstObj, $secondObj, $thirdObj);
+    }
+
+    /**
+     *
+     * @param EqualityInterface $firstObj
+     * @param EqualityInterface $secondObj
+     * @param EqualityInterface $thirdObj
+     *
+     * @return void
+     */
+    private function output(
+        EqualityInterface $firstObj,
+        EqualityInterface $secondObj,
+        EqualityInterface $thirdObj
+    ) {
+        $this->writeLine();
+
+        // Member method
+        $this->writeLine(
+            '$firstObj->equals($firstObj) = ' . $firstObj->equals($firstObj)
+        ); // reflexive!
+        $this->writeLine(
+            '$secondObj->equals($firstObj) = ' . $secondObj->equals($firstObj)
+        );
+        $this->writeLine(
+            '$firstObj->equals($secondObj) = ' . $firstObj->equals($secondObj)
+        ); // symmetric!
+        $this->writeLine(
+            '$secondObj->equals($thirdObj) = ' . $secondObj->equals($thirdObj)
+        );
+        $this->writeLine(
+            '$firstObj->equals($thirdObj) = ' . $firstObj->equals($thirdObj)
+        ); // transitive!
+
+        // Utility class method
+        $this->writeLine(
+            'EqualityUtils::isEqual($firstObj, $secondObj) = '
+            . EqualityUtils::isEqual($firstObj, $secondObj)
+        );
+        $this->writeLine();
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return void
+     */
+    private function writeLine($text = '')
+    {
+        echo $text . \PHP_EOL;
+    }
 }
 
-function demonstrateDefaultImplementation()
-{
-    $firstObj = new EqualityDefaultImpl;
-    $secondObj = new EqualityDefaultImpl;
-    $thirdObj = new EqualityDefaultImpl;
-
-    writeLine('Default implementation:');
-    output($firstObj, $secondObj, $thirdObj);
-}
-
-function output(
-    EqualityInterface $firstObj,
-    EqualityInterface $secondObj,
-    EqualityInterface $thirdObj
-) {
-    writeLine();
-
-    // Member method
-    writeLine('$firstObj->equals($firstObj) = ' . $firstObj->equals($firstObj)); // reflexive!
-    writeLine('$secondObj->equals($firstObj) = ' . $secondObj->equals($firstObj));
-    writeLine('$firstObj->equals($secondObj) = ' . $firstObj->equals($secondObj)); // symmetric!
-    writeLine('$secondObj->equals($thirdObj) = ' . $secondObj->equals($thirdObj));
-    writeLine('$firstObj->equals($thirdObj) = ' . $firstObj->equals($thirdObj)); // transitive!
-
-    // Utility class method
-    writeLine(
-        'EqualityUtils::isEqual($firstObj, $secondObj) = '
-        . EqualityUtils::isEqual($firstObj, $secondObj)
-    );
-    writeLine();
-}
-
-function writeLine($text = '')
-{
-    echo $text . \PHP_EOL;
-}
-
-function demonstrateCustomImplementation()
-{
-    $firstObj = new EqualityCustomImpl;
-    $secondObj = new EqualityCustomImpl(false);
-    $thirdObj = new EqualityCustomImpl(0);
-
-    writeLine('Custom implementation:');
-    output($firstObj, $secondObj, $thirdObj);
-}
+exit(EqualityExample::main($argc, $argv));
