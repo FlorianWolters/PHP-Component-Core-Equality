9c5e741ae07441e89d0bff93396f2742029dbd46
Florian Wolters <wolters.fl@gmail.com>
2013-03-22 13:27:40 +0100

    Updated method comment.

--- src/php/FlorianWolters/Component/Core/EqualityInterface.php
+++ src/php/FlorianWolters/Component/Core/EqualityInterface.php
@@ -16,6 +16,27 @@ interface EqualityInterface
     /**
      * Indicates whether the specified object is "equal to" this object.
      *
+     * The {@see equals} method implements an equivalence relation on non-`null`
+     * object references:
+     *
+     * * It is *reflexive*: for any non-`null` reference value `$x`,
+     *   `$x->equals($x)` should return `true`.
+     * * It is *symmetric*: for any non-`null` reference values `$x` and `$y`,
+     *   `$x->equals($y)` should return `true` if and only if `$y->equals($x)`
+     *   returns `true`.
+     * * It is *transitive*: for any non-`null` reference values `$x`, `$y`, and
+     *   `$z`, if `$x->equals($y)` returns `true` and `$y->equals($z)` returns
+     *   `true`, then `$x->equals($z)` should return `true`.
+     * * It is *consistent*: for any non-`null` reference values `$x` and `$y`,
+     *   multiple invocations of `$x->equals($y)` consistently return `true` or
+     *   consistently return `false`, provided no information used in {@link
+     *   equals} comparisons on the objects is modified.
+     * * For any non-`null` reference value `$x`, `$x->equals(null)` should
+     *   return `false`.
+     *
+     * The {@see equals} method implements the *Equality Method* implementation
+     * pattern.
+     *
      * @param EqualityInterface|null $other The reference object with which to
      *                                      compare.
      *
