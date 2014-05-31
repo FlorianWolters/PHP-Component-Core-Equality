83f75015f389fadfca54c2648eb3ebc94d402cac
Florian Wolters <wolters.fl@gmail.com>
2014-05-31 14:25:12 +0200

    Updated API documentation using the documentation generator "Sami".
    Signed-off-by: Florian Wolters <wolters.fl@gmail.com>

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","FlorianWolters\\Component\\Core\\EqualityInterface"],["c","FlorianWolters\\Component\\Core\\EqualityUtils"],["c","FlorianWolters\\Component\\Core\\ReferenceEqualityTrait"],["c","FlorianWolters\\Component\\Core\\ValueEqualityTrait"]];
---
+++ tree.js
@@ -0,0 +1,9 @@
+var tree = [["FlorianWolters",,"",
+                [["Component",,"",
+                [["Core","FlorianWolters\/Component\/Core.html","",
+                [["EqualityInterface","FlorianWolters\/Component\/Core\/EqualityInterface.html","",
+                [        ]],["EqualityUtils","FlorianWolters\/Component\/Core\/EqualityUtils.html","",
+                [        ]],["ReferenceEqualityTrait","FlorianWolters\/Component\/Core\/ReferenceEqualityTrait.html","",
+                [        ]],["ValueEqualityTrait","FlorianWolters\/Component\/Core\/ValueEqualityTrait.html","",
+                [        ]]        ]]        ]]        ]]        ]
+
